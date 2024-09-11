87cce64b52172861b27a06853760f75de8190c07
VincentTan8 <vincent.tan@wetalk.com>
2024-09-11 11:17:08 +0800

    Test for adding files (#1)
    * feat: add new layouts and 404 pages
    
    * feat: update app and config
    
    * test: add file test
    
    ---------
    
    Co-authored-by: John Jercai Cortejo <oseajicoy>

---
+++ apps/frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        commonjs: true,
+        node: true,
+        es2021: true
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    plugins: ['@typescript-eslint', 'hexagonal-architecture'],
+    extends: [
+        'plugin:@typescript-eslint/recommend',
+        '@vue/typescript/recommend',
+        'eslint-config-custom'
+    ],
+    settings: {
+        'import/resolver': {
+			typescript: {},
+			node: {
+				extensions: ['.js', '.ts', '.vue'],
+			},
+		},
+    },
+    overrides: [
+		{
+			files: ['src/**/*.ts'],
+			rules: {
+				'@typescript-eslint/consistent-type-imports': 1,
+			},
+		},
+	],
+}
---
+++ apps/frontend/pages/[...404].vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="error-page">
+		<p class="error-page__text" v-text="'Page not found'" />
+	</div>
+</template>
+
+<script lang="ts" setup>
+useHead({
+	title: '404',
+})
+</script>
