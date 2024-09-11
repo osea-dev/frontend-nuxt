8c215d04fd7b130327117b761d9fb37a828990cf
VincentTan8 <vincent.tan@wetalk.com>
2024-09-11 14:27:48 +0800

    test: added Test.spec.ts

---
+++ apps/frontend/pages/test/Test.spec.ts
@@ -0,0 +1,11 @@
+import { createTestingPinia } from '@pinia/testing'
+// import { render } from '@test/test.utils'
+
+import Test from './Test.vue'
+
+describe('Test Page', () => {
+    //testing done here
+})
+
+
+
