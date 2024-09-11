063171f04554bab815e867b5e70be2e31afed564
VincentTan8 <vincent.tan@wetalk.com>
2024-09-11 11:12:41 +0800

    test: add file test

---
+++ apps/frontend/pages/test/Test.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    defineI18nRoute({
+        paths: {
+            en:'/test'
+        }
+    })
+</script>
+
+<template>
+    
+    <h1>Hello World!</h1>
+
+</template>
