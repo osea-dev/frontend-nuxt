a600526f57ce4bae7e2b2220816b134be5a12497
oseajicoy <oseadevteam@gmail.com>
2024-09-20 09:26:02 +0800

    fix: add vitest for vi var

--- apps/frontend/test/test.config.ts
+++ apps/frontend/test/test.config.ts
@@ -1,4 +1,6 @@
+/// <reference types="vitest" />
 import { expect, afterEach } from 'vitest'
+import { vi } from 'vitest'
 import { cleanup } from '@testing-library/vue'
 import matchers from '@testing-library/jest-dom/matchers'
 import '@testing-library/jest-dom'
